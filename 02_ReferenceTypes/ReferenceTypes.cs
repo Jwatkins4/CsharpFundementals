31b1a5703d99187f30a3628bd6f293e8757b5dfe
FirstName LastName <Jamarwatkins1@gmail.com>
2021-09-23 17:58:01 -0400

    Add project files.

---
+++ 02_ReferenceTypes/ReferenceTypes.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace _02_ReferenceTypes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Stringsd
+            string thisIsDeclaration;
+            thisIsDeclaration = "This is initialiized.";
+
+            string declarationAndInitialization = "This is both declaring and initializaing a string.";
+
+            //Console.WriteLine("What is your first name?");
+            //string firstName = Console.ReadLine();
+            //Console.WriteLine(firstName);
+
+            //String Operation
+            //Concatenation
+            string concatenatedFullName;
+            string firstName = "Jamar";
+            string lastName = "Watkins";
+            concatenatedFullName = firstName + " " + lastName; //Jamar Watkins
+            Console.WriteLine(concatenatedFullName);
+
+            //Interpolation
+            string interpolatedFullName = $"{firstName} {lastName}";
+            Console.WriteLine(interpolatedFullName);
+
+            //Collection
+            //List
+            List<string> listOfStrings = new List<string>();
+            listOfStrings.Add(firstName);
+            listOfStrings.Add(lastName);
+
+            //Array
+            string[] arrayOfStrings = new string[8];
+            string[] anotherArrayOfStrings = { firstName, lastName, "Jamar" };
+
+            //Queue
+            Queue<string> firstInFirstOut = new Queue<string>();
+            firstInFirstOut.Enqueue(lastName);
+            firstInFirstOut.Enqueue("this Is My String");
+
+            //Dictionary
+            Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
+            keyValuePairs.Add(7, "seven");
+            Console.WriteLine(keyValuePairs[7]);
+
+            //Other Type Collections
+            SortedList<string, int> sortedkeyValuePairs = new SortedList<string, int>();
+            HashSet<int> uniqueList = new HashSet<int>();
+            Stack<string> lastInFirstOut = new Stack<string>();
+
+            //Classes
+            Random randomExample = new Random();
+
+            int randomNumber = randomExample.Next();
+            Console.WriteLine(randomNumber);
+        }
+    }
+}
